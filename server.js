78051eccbdb952cfdfec22a4804d9f6254f83d67
johnston <ethan.john.13579@gmail.com>
2026-06-20 03:06:09 +0400

    yeah man it works ig

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-console.log("hi1!!11")
+const express = require("express");
+const cors = require("cors");
+const nocache = require("nocache");
+const mcp = require("minecraft-protocol");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(nocache());
+
+app.get("/", (req, res) => {
+    console.log(`ok check from ${req.ip}`);
+    res.send("ok");
+});
+
+
+const mcsip = "127.0.0.1"
+const mcsport = 25565
+
+app.get("/players", (req, res) => {
+    console.log(`request to '/players' from ${req.ip}`)
+    mcp.ping({ mcsip, mcsport }, (err, mcres) => {
+        if (err) { return res.json({"error": "server down" }); }
+
+        res.json(mcres);
+    });
+});
+
+app.listen(port, "0.0.0.0", () => {
+    console.log("server up");
+});
+
